61fb1411910b124c5fe8d01aadbf78665cb9d677
raflykunn <rflyrmdhn03@gmail.com>
2026-09-19 13:44:05 +0800

    feat(config): add app configuration, branding assets, and BINFINITY author

---
+++ src/config/app.js
@@ -0,0 +1,12 @@
+export const appConfig = {
+  name: 'Pilahki',
+  tagline: 'Solusi Sampah Terintegrasi',
+  city: 'Makassar',
+  favicon: '/pilahki-favicon.svg',
+  logo: '/pilahki-logo.svg',
+  description: 'Platform terpadu pemilahan, pencarian lokasi bank sampah, jadwal angkut, dan panduan sampah rumah tangga Kota Makassar.',
+  author: 'BINFINITY',
+  version: '1.0.0'
+}
+
+export default appConfig
